69b21b0d105663881bbc7a44f308c80f2565d603
gkganesh95 <gk95520@gmail.com>
2025-10-30 01:03:22 +0530

    Delete weather-app directory

--- weather-app/backend/routes/weatherRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getWeather } from "../controllers/weatherControllers.js";
-const router = express.Router();
-
-router.get("/:city", getWeather);
-
-export default router;
